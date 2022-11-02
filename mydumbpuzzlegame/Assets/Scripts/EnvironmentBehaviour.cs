aeea03793ad4222ad933b939497fbe7831144ff9
NTKiddd <ngotrankhanh163@gmail.com>
2022-11-02 12:50:25 +0700

    1.3.1
    + jump mechanic
    ~ unable jump while mid-air

---
+++ mydumbpuzzlegame/Assets/Scripts/EnvironmentBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentBehaviour : MonoBehaviour
+{
+    private GameObject PlayerMesh;
+    private PlayerMovement playermovementScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayerMesh = GameObject.Find("PlayerMesh");
+        //playermovementScript = PlayerMesh.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- mydumbpuzzlegame/Assets/Scripts/PlayerMovement.cs
+++ mydumbpuzzlegame/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float movementSpeed;
+    public float jumpForce;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool canJump;
+
+    private Rigidbody Rb;
+
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //get input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * movementSpeed * horizontalInput);
+
+        if (canJump)
+            playerJump();
+    }
+
+    void playerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //add force and push player +y (up) 
+            Rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+            canJump == true;
     }
 }
