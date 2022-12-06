371dbfe3fae849161a93e119dd387ac86bd09ddc
NTKiddd <ngotrankhanh163@gmail.com>
2022-12-07 03:00:15 +0700

    6.2.3

--- mydumbpuzzlegame/Assets/Scripts/ThirdPersonCamera.cs
+++ mydumbpuzzlegame/Assets/Scripts/ThirdPersonCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
@@ -8,25 +9,33 @@ public class ThirdPersonCamera : MonoBehaviour
     public GameObject mainCamera;
     public GameObject aimCamera;
     public GameObject aimReticle;
+    public GameObject player;
+    //public Cinemachine.AxisState xAxis;
+    //public Cinemachine.AxisState yAxis;
+
+    public CinemachineFreeLook cinemachineFreeLook;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //cinemachineFreeLook = gameObject.AddComponent<CinemachineFreeLook>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire2"))
+        if (Input.GetMouseButton(1))
         {
-            mainCamera.SetActive(false);
-            aimCamera.SetActive(true);
+            //mainCamera.SetActive(false);
+            //aimCamera.SetActive(true);
+            cinemachineFreeLook.Follow = aimCamera.transform;
+            cinemachineFreeLook.LookAt = aimCamera.transform;
+            //cinemachineFreeLook.
         }
-        else if (Input.GetButtonUp("Fire2"))
+        else if (Input.GetMouseButtonUp(1))
         {
-            mainCamera.SetActive(true);
-            aimCamera.SetActive(false);
+            cinemachineFreeLook.Follow = player.transform;
+            cinemachineFreeLook.LookAt = player.transform;
         }
     }
 }
